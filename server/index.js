1055c0faa2e7c1bc6c3509f799d76cef857e380a
Sandhya <sandhyanaik469@gmail.com>
2024-11-04 12:01:23 +0530

    chat

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use("/api/channel", channelRoutes);
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
 })
 
 
